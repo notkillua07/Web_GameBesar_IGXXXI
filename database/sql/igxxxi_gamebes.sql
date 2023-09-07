8999a034851d76ca7e18ce3c26f718b0e58ba04f
notkillua07 <112600181+notkillua07@users.noreply.github.com>
2023-09-07 19:15:42 +0700

    Update igxxxi_gamebes.sql

--- database/sql/igxxxi_gamebes.sql
+++ database/sql/igxxxi_gamebes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Sep 2023 pada 14.19
+-- Waktu pembuatan: 07 Sep 2023 pada 14.13
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.29
 
@@ -43,7 +43,7 @@ CREATE TABLE `buys` (
 --
 
 INSERT INTO `buys` (`id`, `item_id`, `supplier_id`, `price`, `month`, `demands`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 47, 1, 50000, NULL, '2023-08-31 11:59:49'),
+(1, 1, 1, 47, 1, 50000, NULL, '2023-09-07 12:07:37'),
 (2, 2, 1, 45, 1, 50000, NULL, NULL),
 (3, 3, 1, 61.18, 1, 50000, NULL, NULL),
 (4, 1, 1, 68.6, 2, 50000, NULL, NULL),
@@ -72,7 +72,7 @@ INSERT INTO `buys` (`id`, `item_id`, `supplier_id`, `price`, `month`, `demands`,
 (27, 9, 1, 50, 3, 50000, NULL, NULL),
 (28, 10, 2, 30, 1, 50000, NULL, NULL),
 (29, 11, 2, 32, 1, 50000, NULL, NULL),
-(30, 12, 2, 45, 1, 50000, NULL, NULL),
+(30, 12, 2, 45, 1, 50000, NULL, '2023-09-04 12:44:14'),
 (31, 10, 2, 49.6, 2, 50000, NULL, NULL),
 (32, 11, 2, 32, 2, 50000, NULL, NULL),
 (33, 12, 2, 29, 2, 50000, NULL, NULL),
@@ -337,7 +337,7 @@ CREATE TABLE `sells` (
 --
 
 INSERT INTO `sells` (`id`, `item_id`, `supplier_id`, `price`, `stocks`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 49, 135000, NULL, '2023-09-04 02:03:08'),
+(1, 1, 1, 49, 135000, NULL, '2023-09-06 07:11:32'),
 (2, 2, 1, 47, 135000, NULL, NULL),
 (3, 3, 1, 46, 135000, NULL, NULL),
 (4, 4, 1, 45, 135000, NULL, NULL),
@@ -347,8 +347,8 @@ INSERT INTO `sells` (`id`, `item_id`, `supplier_id`, `price`, `stocks`, `created
 (8, 8, 1, 53, 135000, NULL, NULL),
 (9, 9, 1, 55, 135000, NULL, NULL),
 (10, 10, 2, 32, 135000, NULL, NULL),
-(11, 11, 2, 35, 134900, NULL, '2023-09-04 04:25:55'),
-(12, 12, 2, 30, 135000, NULL, NULL),
+(11, 11, 2, 35, 135000, NULL, '2023-09-04 04:25:55'),
+(12, 12, 2, 30, 135000, NULL, '2023-09-04 05:42:20'),
 (13, 13, 2, 28, 135000, NULL, NULL),
 (14, 14, 2, 25, 135000, NULL, NULL),
 (15, 15, 2, 27, 135000, NULL, NULL),
@@ -444,13 +444,38 @@ CREATE TABLE `teams` (
 --
 
 INSERT INTO `teams` (`id`, `name`, `fulfill_demands`, `debt`, `indebted`, `currency`, `created_at`, `updated_at`) VALUES
-(1, 'Tim 1', 181, 600, 1, 30000, '2023-08-15 08:14:39', '2023-09-04 02:03:08'),
-(2, 'Tim 2', 0, 0, 0, 30000, '2023-09-02 14:59:44', '2023-09-04 04:25:55'),
-(3, 'Tim 3', 0, 0, 0, 30000, '2023-09-02 15:00:25', '2023-09-02 15:00:25'),
-(4, 'Tim 4', 0, 0, 0, 0, '2023-09-02 15:00:45', '2023-09-02 15:00:45'),
-(5, 'Tim 5', 0, 0, 0, 0, '2023-09-02 15:00:45', '2023-09-02 15:00:45'),
-(6, 'Tim 6', 0, 0, 0, 0, NULL, NULL),
-(7, 'Tim 7', 0, 0, 0, 0, NULL, NULL);
+(8, 'Affandra', 0, 0, 0, 30000, NULL, NULL),
+(9, 'Spill The Three', 0, 0, 0, 30000, NULL, NULL),
+(10, 'Beauty and Brain', 0, 0, 0, 30000, NULL, NULL),
+(11, 'Tim AJA', 0, 0, 0, 30000, NULL, NULL),
+(12, 'Tim Pehh', 0, 0, 0, 30000, NULL, NULL),
+(13, 'Tim Cheese', 0, 0, 0, 30000, NULL, NULL),
+(14, 'Tim Edu', 0, 0, 0, 30000, NULL, NULL),
+(15, 'Vander', 0, 0, 0, 30000, NULL, NULL),
+(16, 'Twinkle Triad', 0, 0, 0, 30000, NULL, NULL),
+(17, 'Paseo', 0, 0, 0, 30000, NULL, NULL),
+(18, 'Telor Sakti', 0, 0, 0, 30000, NULL, NULL),
+(19, 'Telor Dadar', 0, 0, 0, 30000, NULL, NULL),
+(20, 'Business Proposal 3.0', 0, 0, 0, 30000, NULL, NULL),
+(21, 'Anak DKV nyasar', 0, 0, 0, 30000, NULL, NULL),
+(22, 'Sinlucky', 0, 0, 0, 30000, NULL, NULL),
+(23, 'CHEERS', 0, 0, 0, 30000, NULL, NULL),
+(24, 'KARUNGGONI', 0, 0, 0, 30000, NULL, NULL),
+(25, 'HOKAY', 0, 0, 0, 30000, NULL, NULL),
+(26, 'Klein and Friends', 0, 0, 0, 30000, NULL, NULL),
+(27, 'Free Willy', 0, 0, 0, 30000, NULL, NULL),
+(28, 'JCA Glory', 0, 0, 0, 30000, NULL, NULL),
+(29, 'SMH', 0, 0, 0, 30000, NULL, NULL),
+(30, 'Clavenfore', 0, 0, 0, 30000, NULL, NULL),
+(31, 'Smala', 0, 0, 0, 30000, NULL, NULL),
+(32, 'Macgyver', 0, 0, 0, 30000, NULL, NULL),
+(33, 'All in Charizz', 0, 0, 0, 30000, NULL, NULL),
+(34, 'CLAVE', 0, 0, 0, 30000, NULL, NULL),
+(35, 'DURIAN MUSANGKING', 0, 0, 0, 30000, NULL, NULL),
+(36, 'Multiverse', 0, 0, 0, 30000, NULL, NULL),
+(37, 'CLAVOTEAM', 0, 0, 0, 30000, NULL, NULL),
+(38, 'Snorlax', 0, 0, 0, 30000, NULL, NULL),
+(39, 'CLAVERS', 0, 0, 0, 30000, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -477,8 +502,39 @@ INSERT INTO `users` (`id`, `name`, `username`, `password`, `role`, `created_at`,
 (2, 'Penpos Penjualan', 'penjualan', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 4, NULL, NULL),
 (3, 'Penpos Distribusi', 'distribusi', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 2, NULL, NULL),
 (4, 'Penpos Hutang', 'hutang', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 3, NULL, NULL),
-(5, 'Tim 1', 'tim1', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 5, NULL, NULL),
-(6, 'Tim 2', 'tim2', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 5, NULL, NULL);
+(7, 'Admin', 'admin', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 0, NULL, NULL),
+(8, 'RefreshAdmin', 'refresh', '$2a$10$3.Rf2zlOsIqokNIyeIWaeeQaIBmeODslGFTGF.NomhAWCxveeg0AK', 0, NULL, NULL),
+(9, 'Affandra', 'affandra', '$2a$12$3hZT4Dmp1E8582bz3wuVXOQp3gX6xiiJknGrY5.raUSrp9HdpEtH.', 5, NULL, NULL),
+(10, 'Spill The Three', 'spillthethree', '$2a$12$okBdCmLa766PYUS8arR4g.nDszBGDQ6ncDXgLsBTrUwFa4YMxEUDm', 5, NULL, NULL),
+(11, 'Beauty and Brain', 'beautyandbrain', '$2a$12$xHSyfNIQd6XuN2ujppXKjelzVdVtgx56vFeSovzuGcs3N7j1EDOJO', 5, NULL, NULL),
+(12, 'Tim AJA', 'timaja', '$2a$12$lfTp//5Edu2dz8y.jSfglOaVgM9j0zWcM8vnjr7PxBBR0Ej24mpQK', 5, NULL, NULL),
+(13, 'Tim Cheese', 'timcheese', '$2a$12$aQl9iTmOud7f0NcmIFRlmev//kyF3nZGnotNm.D94At8c2h7QLck2', 5, NULL, NULL),
+(14, 'Tim Edu', 'timedu', '$2a$10$28W3mG9ScUyroV7kHi1BeON9Y.xfl7Q4IlFBEDTzQkV9gn93S8YMi', 5, NULL, NULL),
+(15, 'Vander', 'vander', '$2a$10$EfB6WGFxv4qQKyZmembVOON29hkkeIfUoXTVGUWJKZf98qfq.Pcnq', 5, NULL, NULL),
+(16, 'Twinkle Triad', 'twinkletriad', '$2a$10$6iNQdUwPa8Fb4PNF13ddSe/MRS/QBMscQb8.XL9MKVb4MIcEWi6Di', 5, NULL, NULL),
+(17, 'Paseo', 'paseo', '$2a$10$WJ3Z5LZZrGVZHRMzP06l3.oe.tGssKnH7QpLXy9kczWkyMeclRrde', 5, NULL, NULL),
+(18, 'Telor Sakti', 'telorsakti', '$2a$10$ELPYUsGWlrFQXwcHZYKJjeKdhSQRKUlaIPd5q/7UX3CQz9js4kpMG', 5, NULL, NULL),
+(19, 'Telor Dadar', 'telordadar', '$2a$10$vhSDywyXhNCXMb/x3hkP1uaGNegT5EtHcUKpVktuVImfL/ymFtWPi', 5, NULL, NULL),
+(20, 'Business Proprosal 3.0', 'businessproposal', '$2a$10$.YqUZ8PwoZHJmmfFeTg2cOdcqXINjwkXh0Ie0DGRftrlkdndFqori', 5, NULL, NULL),
+(21, 'Anak DKV nyasar', 'anakdkvnyasar', '$2a$10$jRfF.kC940bhvvQY5eln0uwismG9RW55dIcuiLJ09NPoQ0fV8mw9C', 5, NULL, NULL),
+(22, 'Sinlucky', 'sinlucky', '$2a$10$7I21LWgmC7O63uA23cXjx.9.ofmjAy2ir.CcwGU5X0kiG6XDzbkMC', 5, NULL, NULL),
+(23, 'CHEERS', 'cheers', '$2a$10$r5kVZdsw7d2dPrPLSyXyZu5TAR1V9a85U0/GWRZJbxogIhK9yT9tO', 5, NULL, NULL),
+(24, 'KARUNGGONI', 'karunggoni', '$2a$10$xGJq7H9UJpT4fszVAttabOGhzDZunCdftgHmFRJWxXm95Hdhe/sDu', 5, NULL, NULL),
+(25, 'HOKAY', 'hokay', '$2a$10$4gqyfgo8TW7UzrXVWmsk...meEYQOZxOvMwZXrJ2vaFqo0uigyQGm', 5, NULL, NULL),
+(26, 'Klein and Friends', 'kleinandfriends', '$2a$10$fC8Kzeeij7DT0O/vy94Ys.4jBwqi6K1FSiSqoVaxGk7hmf4EG0JSq', 5, NULL, NULL),
+(27, 'Free Willy', 'freewilly', '$2a$10$BatduePqRjpvHgEsIu.icex7hANmOt4Zypgb4ENilFTC7s8VJqy.q', 5, NULL, NULL),
+(28, 'JCA Glory', 'jcaglory', '$2a$10$gzg9v8yfDR3Ht/p3qpK9lOvNNqtY1x6WLrQSeFb02N8sJPIBrTFyi', 5, NULL, NULL),
+(29, 'SMH', 'smh', '$2a$10$gvRk7LkCXKe3brpy9edzZePa5F4.iowOP5/c2/4wGZFLZCqnD5cPy', 5, NULL, NULL),
+(30, 'Clavenfore', 'clavenfore', '$2a$10$uwCl05Bl1u4AzMIfVY/V0..NXflPrp2iK5CB7VZgbnIgsifqniMk.', 5, NULL, NULL),
+(31, 'Smala', 'smala', '$2a$10$7WilUIUpHCUKP9wVIiFPyuQx271chxER3kQf2isW5.hzzXySMKsH6', 5, NULL, NULL),
+(32, 'Macgyver', 'macgyver', '$2a$10$XrDFwVtE4HycWKKNRQ77au8ot1rP4R5j0CI3w2EjULecZUu7P5VS2', 5, NULL, NULL),
+(33, 'All in Charizz', 'allincharizz', '$2a$10$PREEhKDK4waXxileTOSyTelMPRa1ccIELxm5n6dJeiocd9NuSMUB.', 5, NULL, NULL),
+(34, 'CLAVE', 'clave', '$2a$10$we1j1HWKaC89Tn.qK7StueYpdp2LcddA59krXidgQe2Ortg1RKQYu', 5, NULL, NULL),
+(35, 'DURIAN MUSANGKING', 'durianmusangking', '$2a$10$CDmQToVgguN9WD193vMA7uDfwk8inPNL3XaHqful.WVmXA.JYhARO', 5, NULL, NULL),
+(36, 'Multiverse', 'multiverse', '$2a$10$Kq3z198Qy.onxp2nreIJ1.Jr9qeNFpZln5LX/I9RHUiyZhxxxj2L2', 5, NULL, NULL),
+(37, 'CLAVOTEAM', 'clavoteam', '$2a$10$qy5H9duX7Yq1jUeLeor.1e.lm70XI0UTbRMs/YFRhYAfYQaqisTmG', 5, NULL, NULL),
+(38, 'Snorlax', 'snorlax', '$2a$10$XAoJU3R2eHO.5y/Q5L2qxOiBdubz/b4CX5cOxoype9I1aP1SK5WsK', 5, NULL, NULL),
+(39, 'CLAVERS', 'clavers', '$2a$10$mliSJfVprptVFqxvhb0MUugllYYWGKnd3FuzIBBirDeE8qmR774Z2', 5, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -595,7 +651,7 @@ ALTER TABLE `buys`
 -- AUTO_INCREMENT untuk tabel `buy_transactions`
 --
 ALTER TABLE `buy_transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `expeditions`
@@ -613,7 +669,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT untuk tabel `inventories`
 --
 ALTER TABLE `inventories`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT untuk tabel `items`
@@ -637,7 +693,7 @@ ALTER TABLE `sells`
 -- AUTO_INCREMENT untuk tabel `sell_transactions`
 --
 ALTER TABLE `sell_transactions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `sessions`
@@ -655,13 +711,13 @@ ALTER TABLE `suppliers`
 -- AUTO_INCREMENT untuk tabel `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
