b4db48fc84840ec5c390f6cf373f2fd820542823
CrITicalHit <anthoniusevan@gmail.com>
2023-09-08 19:22:43 +0700

    Mempercantik layout admin

--- resources/views/penpos/admin.blade.php
+++ resources/views/penpos/admin.blade.php
@@ -6,10 +6,10 @@
 
 @section('content')
     {{-- Outer Card --}}
-    <div class="card shadow mt-3 mx-3">
+    <div class="card shadow mt-5 mx-3">
 
         {{-- Outer Card Head --}}
-        <div class="card-header text-center" style="background-color:#7DC1E3;">
+        <div class="card-header text-center pt-3" style="background-color:#7DC1E3;">
             <h1 style="font-weight:bold;color:#ffffff;"><i class="bi bi-person-badge-fill"></i> Admin Area</h1>
         </div>
 
@@ -32,28 +32,28 @@
             {{-- Tabel --}}
             <div class="card m-auto mt-5 shadow-sm">
                 <div class="card-header px-4" style="background-color:#7DC1E3;">
-                    <div class="row p-auto pt-2">
-                        <div class="col-sm">
+                    <div class="row p-auto">
+                        <div class="col-auto pt-2">
                             <h1 class="text-left" style="color:#ffffff;"><i class="bi bi-calculator"></i> Hutang</h1>
                         </div>
-                        <div class="col-sm pe-5">
-                            <h1 class="text-end" id="koin" style="color:#ffffff;">
-                                0
+                        <div class="col-sm pe-3 border-end border-2 border-white pt-2">
+                            <h1 class="text-end pe-2 ps-2" id="koin" style="color:#2C56A7; background-color:#C3E2F6; border-radius: 20px;">
+                                30000
                             </h1>
                         </div>
-                        <div class="col-sm">
+                        <div class="col-auto ps-3 pt-2">
                             <h1 class="text-left" style="color:#ffffff;"><i class="bi bi-coin"></i> Total Dana</h1>
                         </div>
-                        <div class="col-sm pe-5">
-                            <h1 class="text-end" id="hutang" style="color:#ffffff;">
+                        <div class="col-sm pe-3 pt-2 border-end border-2 border-white">
+                            <h1 class="text-end pe-2 ps-2" id="hutang" style="color:#2C56A7; background-color:#C3E2F6; border-radius: 20px;">
                                 0
                             </h1>
                         </div>
-                        <div class="col-sm ps-5">
+                        <div class="col-auto ps-3 pt-2">
                             <h1 class="text-left" style=";color:#ffffff;"><i class="bi bi-boxes"></i> Demand</h1>
                         </div>
-                        <div class="col-sm">
-                            <h1 class="text-end" id="demands" style="color:#ffffff;">
+                        <div class="col-sm pt-2">
+                            <h1 class="text-end pe-2 ps-2" id="demands" style="color:#2C56A7; background-color:#C3E2F6; border-radius: 20px;">
                                 0
                             </h1>
                         </div>
@@ -80,7 +80,7 @@
 
     {{-- Control Area --}}
     <div class="card mt-3 mx-auto text-center" style="height: 20em; width:25em">
-        <div class="card-header text-center" style="background-color:#7DC1E3;">
+        <div class="card-header text-center pt-3" style="background-color:#7DC1E3;">
             <h1 style="font-weight:bold;color:#ffffff;"><i class="bi bi-joystick"></i></i> Control Area</h1>
         </div>
         <div class="card-body">
